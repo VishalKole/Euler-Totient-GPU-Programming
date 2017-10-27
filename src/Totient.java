19641bd4c9e7de140ada84fb0a4d3bf4f00613fb
Vishal Kole <vishalvkole@gmail.com>
2017-10-26 21:17:18 -0400

    euler totient works with test case

--- src/Totient.java
+++ src/Totient.java
@@ -2,6 +2,15 @@ public class Totient {
 
     public static void main(String[] args){
 
+        Totient t = new Totient();
+        int sum =0;
+        int N=100;
+        for(int i=0;i<N;i++){
+            if( t.GCD(i,N)==1 ){
+                ++sum;
+            }
+        }
+        System.out.println(sum);
 
     }
 
