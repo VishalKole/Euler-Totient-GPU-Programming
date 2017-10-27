c6a64f789769a88d2506a7d4eacab9d2a5448395
Vishal Kole <vishalvkole@gmail.com>
2017-10-27 17:41:26 -0400

    Totient.java almost complete. needs to check and modify the logic

--- src/Totient.cu
+++ src/Totient.cu
@@ -7,7 +7,7 @@ __device__ unsigned long long int devCount;
 // Per-thread counter variables in shared memory.
 __shared__ unsigned long long int shrCount [NT];
 
-extern "C" __global__ void ComputeTotient(unsigned long long int P, unsigned long long int N){
+extern "C" __global__ void ComputeTotient(unsigned long long int N){
 
     int thr, size, rank;
     unsigned long long int count;
@@ -16,36 +16,32 @@ extern "C" __global__ void ComputeTotient(unsigned long long int P, unsigned lon
     size = gridDim.x*NT;
     rank = blockIdx.x*NT + thr;
 
-
- count = 0;
-
+    count = 0;
     int temp;
+
     for (unsigned long long int i = rank; i < N; i += size){
-            while (q != 0) {
-                temp = q;
-                q = p % q;
-                p = temp;
-            }
-            if(p==1)
-            ++count;
-}
+        while (rank != 0) {
+            temp = rank;
+            rank = p % rank;
+            p = temp;
+        }
+        if(p==1)
+        ++count;
+    }
 
  // Shared memory parallel reduction within thread block.
-   shrCount[thr] = count;
-   __syncthreads();
+    shrCount[thr] = count;
+    syncthreads();
 
-   for (int i = NT/2; i > 0; i >>= 1)
-         {
-         if (thr < i)
-            shrCount[thr] += shrCount[thr+i];
-         __syncthreads();
-         }
+    for (int i = NT/2; i > 0; i >>= 1){
 
-            // Atomic reduction into overall counter.
-            if (thr == 0)
-               atomicAdd (&devCount, shrCount[0]);
+        if (thr < i)
+        shrCount[thr] += shrCount[thr+i];
+        syncthreads();
+    }
 
+    // Atomic reduction into overall counter.
+    if (thr == 0)
+    atomicAdd (&devCount, shrCount[0]);
 
-
-
-       }
+}
--- src/Totient.java
+++ src/Totient.java
@@ -7,34 +7,51 @@ import edu.rit.pj2.Task;
 
 public class Totient extends Task {
 
-    long N=100;
+    /**
+     * Kernel function interface.
+     */
+    private static interface TotientKernel
+            extends Kernel {
+        public void ComputeTotient
+                (long N);
+    }
 
-    public void main(String[] args) throws Exception{
+    public void main(String[] args) throws Exception {
+
+        long N = Long.parseLong(args[0]);
 
         // Initialize GPU.
         Gpu gpu = Gpu.gpu();
-        gpu.ensureComputeCapability (2, 0);
+        gpu.ensureComputeCapability(2, 0);
 
         // Set up GPU counter variable.
-        Module module = gpu.getModule ("/f/Totient.ptx");
-        GpuLongVbl count = module.getLongVbl ("devCount");
+        Module module = gpu.getModule("/f/Totient.ptx");
+        GpuLongVbl count = module.getLongVbl("devCount");
 
+        count.item = 0;
+        count.hostToDev();
+        TotientKernel kernel = module.getKernel(TotientKernel.class);
+        kernel.setBlockDim(1024);
+        kernel.setGridDim(gpu.getMultiprocessorCount());
+        kernel.ComputeTotient(N);
+
+        // Print results.
+        count.devToHost();
+        System.out.printf("pi = %d", count.item);
 
     }
 
     /**
      * Specify that this task requires one core.
      */
-    protected static int coresRequired()
-    {
+    protected static int coresRequired() {
         return 1;
     }
 
     /**
      * Specify that this task requires one GPU accelerator.
      */
-    protected static int gpusRequired()
-    {
+    protected static int gpusRequired() {
         return 1;
     }
 }
