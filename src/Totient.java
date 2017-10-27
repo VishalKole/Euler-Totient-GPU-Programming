424e75c256cbb962b242b473015161b6c10e5c99
Vishal Kole <vishalvkole@gmail.com>
2017-10-26 21:09:25 -0400

    GCD function created

--- src/Totient.java
+++ src/Totient.java
@@ -1,2 +1,17 @@
 public class Totient {
+
+    public static void main(String[] args){
+
+
+    }
+
+    public int GCD(int p, int q){
+        while (q != 0) {
+            int temp = q;
+            q = p % q;
+            p = temp;
+        }
+        return p;
+    }
+
 }
