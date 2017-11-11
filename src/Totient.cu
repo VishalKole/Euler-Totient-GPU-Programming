bdd496cd6f61b4ea43a34970d7512392ce9c0652
Vishal Kole <vishalvkole@gmail.com>
2017-11-11 18:17:37 -0500

    final program

--- src/Totient.cu
+++ src/Totient.cu
@@ -7,15 +7,21 @@ __device__ unsigned long long int devCount;
 // Per-thread counter variables in shared memory.
 __shared__ unsigned long long int shrCount [NT];
 
+//Kernel function to compute the Totient value. It just takes the number on which the
+//totient should be computed and computes the totient for the current thread number +
+//size of the totals threads the GPU can create.
 extern "C" __global__ void ComputeTotient(unsigned long long int N){
 
+    //Variable declarations
     int thr, size, rank;
     unsigned long long int count=0, b, temp, a;
 
+    //Rank and size computations
     thr = threadIdx.x;
     size = gridDim.x*NT;
     rank = blockIdx.x*NT + thr;
 
+    //loop to compute the totient for current thread rank
     for (unsigned long long int x = rank; x < N; x += size){
     a=x;
     b=N;
@@ -24,23 +30,23 @@ extern "C" __global__ void ComputeTotient(unsigned long long int N){
             b = a % b;
             a = temp;
         }
+
         if(a==1)
         ++count;
     }
 
- // Shared memory parallel reduction within thread block.
+ // assigning the counter value to the shared memory variable
     shrCount[thr] = count;
-    syncthreads();
+    __syncthreads();
 
+    //reduction of the shared variables in parallel
     for (int i = NT/2; i > 0; i >>= 1){
-
         if (thr < i)
         shrCount[thr] += shrCount[thr+i];
-        syncthreads();
+        __syncthreads();
     }
 
-    // Atomic reduction into overall counter.
+    // Adding the total of each block to global variable
     if (thr == 0)
     atomicAdd (&devCount, shrCount[0]);
-
 }
--- src/Totient.java
+++ src/Totient.java
@@ -5,59 +5,86 @@ import edu.rit.gpu.GpuLongVbl;
 import edu.rit.gpu.Module;
 import edu.rit.gpu.CacheConfig;
 
-
+/**
+ * Class Totient computes the Totient value for a long value N
+ */
 public class Totient extends Task {
 
     /**
-     * Kernel function interface.
+     * Interface that relates the Java function to the kernel function
      */
-    private static interface TotientKernel
-            extends Kernel {
-        public void ComputeTotient
-                (long N);
+    private static interface TotientKernel extends Kernel {
+        public void ComputeTotient (long N);
     }
 
+    /**
+     * function sets the parameters to the kernel function and calls the kernel
+     *
+     * @param args the Long integer on which the Totient is computed
+     * @throws Exception if input is invalid
+     */
     public void main(String[] args) throws Exception {
 
+        //validates the input and throws error if not correct.
         long N = ValidateInput(args);
 
         // Initialize GPU.
         Gpu gpu = Gpu.gpu();
+
+        //ensures compute capability is met
         gpu.ensureComputeCapability(2, 0);
 
         // Set up GPU counter variable.
         Module module = gpu.getModule("Totient.ptx");
         GpuLongVbl count = module.getLongVbl("devCount");
 
+        //transferring the variable to the GPU
         count.item = 0;
         count.hostToDev();
+
+        //sets the kernel and the class file which should compute the totient
         TotientKernel kernel = module.getKernel(TotientKernel.class);
         kernel.setBlockDim(1024);
         kernel.setGridDim(gpu.getMultiprocessorCount());
+
+        //Priority is given to the shared memory as it requires only shared and no cache
         kernel.setCacheConfig (CacheConfig.CU_FUNC_CACHE_PREFER_SHARED);
+
+        //calls kernel to compute
         kernel.ComputeTotient(N);
 
-        // Print results.
+        // Print results
         count.devToHost();
         System.out.println(count.item);
 
     }
 
+    /**
+     * Validates the input of the function.
+     * @param input the String input given to the program
+     * @return returns Long integer N if valid
+     * @throws Exception if the input is not valid
+     */
     private long ValidateInput(String input[]) throws Exception {
 
+        //Argument is only one
         if (input.length != 1)
             throw new IllegalArgumentException("Arguments are not exactly equal to one.");
 
+        //it is only numerical in characters
         if (!input[0].matches("[0-9]+"))
             throw new IllegalArgumentException("Argument is not a number and contains other characters or" +
                     " special characters.");
         long number;
         try{
+            //checks if it is Long integer
             number = Long.parseLong(input[0]);
         }
         catch (Exception e){
             throw new IllegalArgumentException("Value should be in the range from 1 to 9,223,372,036,854,775,808. ");
         }
+
+        //checks if the value is negative
         if ((number < 1))
             throw new IllegalArgumentException("Invalid number to calculate Totient. should be greater than 0. ");
 
@@ -65,14 +92,16 @@ public class Totient extends Task {
     }
 
     /**
-     * Specify that this task requires one core.
+     * restricting the core to 1
+     * @return 1 always
      */
     protected static int coresRequired() {
         return 1;
     }
 
     /**
-     * Specify that this task requires one GPU accelerator.
+     * restricting the GPU to 1
+     * @return 1 always
      */
     protected static int gpusRequired() {
         return 1;
